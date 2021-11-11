ae4b57158798b225500d4b95fb59d6cbaeca4d2f
victor <vhrc@cin.ufpe.br>
2021-11-11 17:08:22 -0300

    game over

--- script.js
+++ script.js
@@ -40,20 +40,27 @@ function jump() {
 function createCactus() {
   const cactus = document.createElement("div");
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
 
   cactus.classList.add("cactus");
-  cactus.style.left = 1000 + "px";
   background.appendChild(cactus);
+  cactus.style.left = 1000 + "px";
 
   let leftInterval = setInterval(() => {
     if (cactusPosition < -60) {
       clearInterval(leftInterval);
       background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      //gameover
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + "px";
     }
   }, 20);
+
+  setTimeout(createCactus(), randomTime);
 }
 
 createCactus();
