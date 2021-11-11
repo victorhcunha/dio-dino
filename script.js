41479b3c4775d374d0e818593914935bed4f5b76
victor <vhrc@cin.ufpe.br>
2021-11-11 13:28:49 -0300

    img dino

--- script.js
+++ script.js
@@ -3,8 +3,32 @@ console.log(dino);
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
+    jump();
     console.log("Espaço");
   }
+}
+
+function jump() {
+  let position = 0;
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      //down
+      clearInterval(upInterval);
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+        } else {
+          //up
+          position += 20;
+          dino.style.bottom = position + "px";
+        }
+      });
+    } else {
+      //up
+      position += 20;
+      dino.style.bottom = position + "px";
+    }
+  }, 20);
 }
 
 document.addEventListener("keyup", handleKeyUp);
